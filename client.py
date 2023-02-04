72b03830c46f7c66c127118a344c4c1a49d31102
zapper-95 <zapper-95@github.com>
2023-02-04 16:55:40 +0000

    broken, but I want to test

--- client.py
+++ client.py
@@ -1,16 +1,30 @@
 import socket
+import threading
+import sys
+def receive_message(client_socket):
+    while True:
+        modifiedMessage, serverAddress = client_socket.recvfrom(1024)
+        if(modifiedMessage.decode()):
+            print(modifiedMessage.decode())
 
-def start_client():
+def start_client(username):
     serverName = "127.0.0.1"
     serverPort = 8080
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     clientSocket.connect((serverName, serverPort))
-    message = input("Input lowercase sentence: ")
+    clientSocket.send(username.encode())
+    receive_thread = threading.Thread(target=receive_message, args=(clientSocket,))
+    receive_thread.start()
 
-    clientSocket.send(message.encode())
-    modifiedMessage, serverAddress = clientSocket.recvfrom(1024)
-    print(modifiedMessage.decode())
+    while True:
+        try:
+            message = input("Input lowercase sentence: ")
+            clientSocket.sendall(message.encode())
+        except:
+            print("Client disconnected")
+            break
     clientSocket.close()
 
+
 if __name__ == "__main__":
-    start_client()
+    start_client(sys.argv[1])
--- server.py
+++ server.py
@@ -2,12 +2,31 @@ import socket
 import sys
 import threading
 
+
+current_connections = {}
 def handle_clients(client_socket, client_address):
-    print("Connected to {}".format(client_address))
-    message = client_socket.recv(1024)
-    print(message.decode())
-    client_socket.send("dicks".encode())
-    client_socket.close()
+    print("Connected to from {}".format(client_address))
+    client_socket.send("Welcome to the server".encode())
+
+    while True:
+        try:
+            message = client_socket.recv(1024)
+            #check if message is empty and if it isn't pass it to send_all()
+            #if message.decode():
+                #send_all(message)
+                #print("bitch")
+            #client_socket.send("dicks".encode())
+        except:
+            print("Client disconnected")
+            client_socket.close()
+            return
+
+
+def send_to_all_connections(message):
+    #write code to send a message to all clients connected to the server
+    for connection in current_connections.values:
+        connection.send("fuck")
+    
 
 def start_server(port):
     serverPort = port
@@ -17,9 +36,19 @@ def start_server(port):
     serverSocket.listen()
     print("The server is ready to receive")
     while True:
-        clientSocket, clientAddress = serverSocket.accept()
-        client_thread = threading.Thread(target=handle_clients, args=(clientSocket, clientAddress))
-        client_thread.start()
+        try:
+            clientSocket, clientAddress = serverSocket.accept()
+            username = clientSocket.recv(1024).decode() #send username as firs thing
+            current_connections[clientAddress] = clientSocket
+            clientSocket.send("Fuck")
+            send_to_all_connections("{} has joined the server".format(username))
+            #client_thread = threading.Thread(target=handle_clients, args=(clientSocket, clientAddress))
+            #client_thread.start()
+            
+        except:
+            print("Server shutting down")
+            break
+    serverSocket.close()
 
 if __name__=="__main__":
     #take in argument port, which is then passed into start_server()
