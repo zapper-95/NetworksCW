ea7f818b10e7ba6f2fc67d6ed13977b78276a6cd
zapper-95 <zapper-95@github.com>
2023-02-05 21:07:55 +0000

    major disrespect

--- client.py
+++ client.py
@@ -1,32 +1,50 @@
 import socket
 import threading
 import sys
-import getpass
+
+
+
 def receive_message(client_socket):
     while True:
-        modifiedMessage, serverAddress = client_socket.recvfrom(1024)
-        if(modifiedMessage.decode()):
-            print(modifiedMessage.decode())
+        try:
+            modifiedMessage = client_socket.recv(1024).decode()
+            print(modifiedMessage)
+        except:
+            sys.exit(1)
+            
+#send message thread function
+def send_message(client_socket):
+    while True:
+        message = input()
+        if message == "q":
+            client_socket.sendall(message.encode())
+            return
+        else:
+            try:
+                client_socket.sendall(message.encode())
+            except:
+                print("could not send message")
 
 def start_client(username):
     serverName = "127.0.0.1"
     serverPort = 8080
-    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    clientSocket.connect((serverName, serverPort))
-    clientSocket.send(username.encode())
-    receive_thread = threading.Thread(target=receive_message, args=(clientSocket,))
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((serverName, serverPort))
+    client_socket.send(username.encode()) #send username as first message to server
+
+    receive_thread = threading.Thread(target=receive_message, args=(client_socket,))
+    
+
+    send_thread = threading.Thread(target=send_message, args=(client_socket,))
+
     receive_thread.start()
+    send_thread.start()
 
-    while True:
-        try:
-            message = input(username + ": ")
-            #message = getpass.getpass("Input something: ")
-            clientSocket.sendall(message.encode())
-        except:
-            print("Client disconnected")
-            clientSocket.close()
-            sys.exit()
-
+    #close both threads
+    receive_thread.join()
+    send_thread.join()
+    #stop program
+    client_socket.close()
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -2,23 +2,35 @@ import socket
 import sys
 import threading
 
+run_server = True
 
 current_connections = {}
+
+def quit_client(client_address, username):
+        current_connections[client_address].close()
+        
+        send_to_all_connections("{} has left the server".format(username), current_connections[client_address])
+
+        del current_connections[client_address]
+        print("Client disconnected")
+        return
+
+
 def handle_clients(client_socket, client_address, username):
     print("Connected to from {}".format(client_address))
     client_socket.send("Welcome to the server!".encode())
     try:
         while True:
+            message = client_socket.recv(1024).decode()
+            if message == "q":
+                quit_client(client_address, username)
+                return
 
-            message = client_socket.recv(1024)
-            if message.decode():
-                print(message.decode())
-                send_to_all_connections(username + ": " + message.decode(), client_socket)
+            else:
+                print(message)
+                send_to_all_connections(username + ": " + message, client_socket)
     except:
-        del current_connections[client_address]
-        send_to_all_connections("{} has left the server".format(username), client_socket)
-        print("Client disconnected")
-        client_socket.close()
+        quit_client(client_address, username)
         return
 
 
@@ -37,9 +49,10 @@ def start_server(port):
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
     serverSocket.bind(("127.0.0.1", serverPort))
     serverSocket.listen()
+    
     print("The server is ready to receive")
     try:
-        while True:
+        while run_server:
                 clientSocket, clientAddress = serverSocket.accept()
                 username = clientSocket.recv(1024).decode() #send username as firs thing
                 current_connections[clientAddress] = clientSocket
@@ -47,11 +60,11 @@ def start_server(port):
                 send_to_all_connections("{} has joined the server".format(username), clientSocket)
                 client_thread = threading.Thread(target=handle_clients, args=(clientSocket, clientAddress, username))
                 client_thread.start()
-
+                print("fuck")
             
     except:
         print("Server shutting down")
-    serverSocket.close()
+        serverSocket.close()
 
 if __name__=="__main__":
     #take in argument port, which is then passed into start_server()
